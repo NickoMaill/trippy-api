e3434c9eaf456e7969ecaad10f048e23b4159bc9
Nicko <nickos@iMac.local>
2022-03-14 12:37:22 +0100

    add routes folder -  restaurantRouter.js and creat Joi verify

---
+++ Routes/restaurantsRouter.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const Joi = require("joi");
+
+const restaurant = Joi.object({
+    name: Joi.string().max(30).required(),
+    address: Joi.string().alphanum().max(50).required(),
+    city: Joi.string().max(30).required(),
+    country: Joi.string().max(30).required(),
+    stars: Joi.number.min(1).max(5).required(),
+    cuisine: Joi.string().max(30).required(),
+    priceCategory: Joi.number().min(1).max(3),
+})
+
