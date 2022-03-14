e602f27b4d8cc021432bbe94d1ca4260350ebb31
Nicko <nickos@iMac.local>
2022-03-14 15:25:01 +0100

    add function middleware for validate new hotel and add auto id

--- Routes/hotelRouter.js
+++ Routes/hotelRouter.js
@@ -11,7 +11,7 @@ const hotel = Joi.object({
     country: Joi.string().required(),
     stars: Joi.number().min(1).max(5).required(),
     hasSpa: Joi.boolean().required(),
-    hosPool: Joi.boolean().required(),
+    hasPool: Joi.boolean().required(),
     priceCategory:Joi.number().min(1).max(3).required(),
 })
 
@@ -25,6 +25,20 @@ function findId(req, res, next) {
     res.json(hotel)
 	next();
 }
+//Function to validate hotel adding
+function validHotel(req, res, next) {
+    const validation = hotel.validate(req.body)
+
+    if (validation.error) {
+        return res.status(400).json({
+            message: "Error 400",
+            description: validation.error.details[0].message,
+        })
+    }
+
+    next();
+}
+
 //Route to get all hotels
 router.get("/hotels", (_req, res) => {
 
@@ -41,5 +55,40 @@ router.get("/hotels", (_req, res) => {
 router.get("/hotels/:id", findId, (req, res) => {
     res.json(req.id)
 })
+
+router.post("/hotels",validHotel, (req, res)=> {
+    console.log("request received");
+
+    hotels.push({
+        id: hotels.length + 1,
+        name: req.body.name,
+        address: req.body.address,
+        city: req.body.city,
+        country: req.body.country,
+        stars: req.body.stars,
+        hasSpa: req.body.hasSpa,
+        hasPool: req.body.hasPool,
+        priceCategory: req.body.priceCategory,
+    });
+
+    res.status(201).json({
+        message: "Hotel added",
+        hotels
+    })
+})
+
+//copy and paste it postman
+
+// {
+//     "id": 1,
+//     "name": "Hilton Paris Opera",
+//     "address": "108 rue Saint Lazare",
+//     "city": "Paris",
+//     "country": "France",
+//     "stars": 5,
+//     "hasSpa": true,
+//     "hasPool": true,
+//     "priceCategory": 3
+// }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 // Import
 const express = require("express");
 const app = express();
-const hotelRouter = require("./Routes/hotelRouter")
+
+//Routers
+const hotelRouter = require("./Routes/hotelRouter");
+
+//Server PORT
 const PORT = 8000;
 
 //Middlewares
 app.use(express.json());
 
+//Routes
+
 app.get("/", (_req, res) => {
     res.send("Welcome on Trippy API")
 })
-
-//Routes
 app.get("/hotels", hotelRouter);
 
-app.get("/hotels/:id", hotelRouter)
+app.post("/hotels", hotelRouter);
 
 //Server Listener
 app.listen(PORT, () => console.log("Listening on port 8000"));
