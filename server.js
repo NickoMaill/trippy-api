c45513957e0a96ee2f8f060ed32fd5a08768e236
Nicko <nickos@iMac.local>
2022-03-15 15:13:47 +0100

    add comment in server.js

--- server.js
+++ server.js
@@ -10,28 +10,43 @@ const restaurantRouter = require("./Routes/restaurantsRouter")
 const PORT = 8000;
 
 app.use(express.json());
-app.use("/hotels", hotelRouter); 
 
-//Routes
+//************************************ ROUTES ****************************//
 
 // On Api Load
 app.get("/", (_req, res) => {
     res.send("Welcome on Trippy API")
 })
 
-// HOTELS
+//--------HOTELS---------//
+
+//GET
 app.get("/hotels", hotelRouter); //Display all hotels
+
+//POST
 app.post("/hotels", hotelRouter); //add hotel
+
+//PATCH
 app.patch("/hotels/:id/name", hotelRouter); //update hotel name
+
+//DELETE
 app.delete("/hotels/:id", hotelRouter); //delete hotel
 
-//RESTAURANT
+//---------------RESTAURANT-------------//
+
+//GET
 app.get("/restaurants", restaurantRouter); //Dipslay all restaurant
 app.get("/restaurants/:country", restaurantRouter); // display restaurant by country
 app.get("/restaurants/price/:price", restaurantRouter) //display restaurant by price range
 app.get("/restaurants/cuisine/:cuisine", restaurantRouter) // display restaurant by cuisine type
+
+//POST
 app.post("/restaurants", restaurantRouter); // add a restaurant
+
+//PATCH
 app.patch("/restaurants/:id/name", restaurantRouter); // modify restaurant name
+
+//DELETE
 app.delete("/restaurants/:id", restaurantRouter); // delete a restaurant
 
 // Handle errors
